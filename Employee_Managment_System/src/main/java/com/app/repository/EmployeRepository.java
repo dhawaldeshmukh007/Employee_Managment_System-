f06bfc8bbc94fe0283c2ba9ae2b4f40f8e639efa
Dhawal <Dhawal@DESKTOP-G05BL35>
2022-06-27 16:17:15 +0530

    Repository layer

--- Employee_Managment_System/src/main/java/com/app/repository/EmployeRepository.java
+++ Employee_Managment_System/src/main/java/com/app/repository/EmployeRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.app.entity.Employee;
 
+/*Here We Create Repository layer that is DAO layer*/
+
 public interface EmployeRepository extends CrudRepository<Employee, Integer> {
+	
 	public Employee findById(int id);
 	public Employee findByEmail(String email);
 	public Employee findByName(String name);
