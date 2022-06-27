36d511bb261ad3dc22ff82687495c0916e581826
Dhawal <Dhawal@DESKTOP-G05BL35>
2022-06-27 16:21:52 +0530

    Service Layer

--- Employee_Managment_System/src/main/java/com/app/service/EmployeeService.java
+++ Employee_Managment_System/src/main/java/com/app/service/EmployeeService.java
@@ -8,18 +8,25 @@ import org.springframework.stereotype.Component;
 import com.app.entity.Employee;
 import com.app.repository.EmployeRepository;
 
+/*Here We Create Service Layer */
+
 @Component
 public class EmployeeService {
 
 	@Autowired
 	EmployeRepository employeRepository;
 
+	/* For Getting All Employees Details */ 
+	
 	public List<Employee> getAllEmployee() {
 		List<Employee> list = (List<Employee>) this.employeRepository.findAll();
 
 		return list;
 	}
 
+	
+	/* For Getting All Employee Details by ID */ 
+	
 	public Employee getEmployeeById(int id) {
 		Employee employee = null;
 		employee = this.employeRepository.findById(id);
@@ -27,6 +34,7 @@ public class EmployeeService {
 		return employee;
 	}
 	
+	/* For Getting Email of Employees  */ 
 	public Employee getEmployeeByEmail(String email) {
 		Employee employee = null;
 		employee = this.employeRepository.findByEmail(email);
@@ -34,6 +42,7 @@ public class EmployeeService {
 		return employee;
 	}
 	
+	/* For Getting Name of Employees  */ 
 	public Employee getEmployeeByName(String name) {
 		Employee employee = null;
 		employee = this.employeRepository.findByName(name);
@@ -41,6 +50,7 @@ public class EmployeeService {
 		return employee;
 	}
 
+	/* To Add the Employees  */ 
 	public Employee addEmployee(Employee e) {
 
 		Employee result = this.employeRepository.save(e);
@@ -48,9 +58,13 @@ public class EmployeeService {
 
 	}
 
+	/* To Delete the Employees  */ 
+	
 	public void deleteEmployee(int id) {
 		employeRepository.deleteById(id);
 	}
+	
+	/* To Update the Employees  */ 
 
 	public Employee update(Employee employee) {
 		List<Employee> list = (List<Employee>) this.employeRepository.findAll();
